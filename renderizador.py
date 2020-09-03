68d6219ef2f427ed4e347947e48c1d12188ecce4
guizg <guizgraicer@gmail.com>
2020-09-03 10:22:02 -0300

    p1 done

--- renderizador.py
+++ renderizador.py
@@ -7,20 +7,96 @@ import x3d          # Faz a leitura do arquivo X3D, gera o grafo de cena e faz t
 import interface    # Janela de visualização baseada no Matplotlib
 import gpu          # Simula os recursos de uma GPU
 
+def lineCrossLine(x0, y0, x1, y1, x2, y2, x3, y3):
+    if (max(x0,x1) < min(x2,x3)):
+        return False 
+    
+    a1 = (y0-y1)/(x0-x1)
+    a2 = (y2-y3)/(x2-x3)
+    b1 = y0 - a1*x0
+    b2 = y2 - a2*x2
+
+    if a1 == a2:
+        return False
+    
+    xa = (b2 - b1) / (a1 - a2)
+
+    if ( (xa < max( min(x0,x1), min(x2,x3) )) or
+        (xa > min( max(x0,x1), max(x2,x3) )) ):
+        return False 
+    else:
+        return True
+
+def shouldPaintPixel(x, y, x0, y0, x1, y1):
+    diamondPoints = [(x+0.5, y), (x, y+0.5), (x+1, y+0.5), (x+0.5, y+1)]
+    diamondLines = [[diamondPoints[0], diamondPoints[2]], [diamondPoints[2], diamondPoints[3]], [diamondPoints[3], diamondPoints[1]], [diamondPoints[1], diamondPoints[0]]]
+
+    for line in diamondLines:
+        if lineCrossLine(x0, y0, x1, y1, line[0][0], line[0][1], line[1][0], line[1][1]):
+            return True
+    
+    return False
+
 def polypoint2D(point, color):
     """ Função usada para renderizar Polypoint2D. """
-    gpu.GPU.set_pixel(3, 1, 255, 0, 0) # altera um pixel da imagem
-    # cuidado com as cores, o X3D especifica de (0,1) e o Framebuffer de (0,255)
+    color = [color[0]*255, color[1]*255, color[2]*255]
+
+    for i in range(len(point)):
+        point[i] = int(point[i])
+
+    for x in range(gpu.GPU.width):
+        for y in range(0, gpu.GPU.height):
+            x_point_index = 0
+            y_point_index = 1
+
+            while x_point_index != len(point):
+                if point[x_point_index] == x and point[y_point_index] == y:
+                     gpu.GPU.set_pixel(x, y, color[0], color[1], color[2])
+
+                x_point_index += 2
+                y_point_index += 2
 
 def polyline2D(lineSegments, color):
     """ Função usada para renderizar Polyline2D. """
-    x = gpu.GPU.width//2
-    y = gpu.GPU.height//2
-    gpu.GPU.set_pixel(x, y, 255, 0, 0) # altera um pixel da imagem
+    
+    color = [color[0]*255, color[1]*255, color[2]*255]
+
+    for x in range(gpu.GPU.width):
+        for y in range(gpu.GPU.height):
+            if shouldPaintPixel(x, y, lineSegments[0], lineSegments[1], lineSegments[2], lineSegments[3]):
+                gpu.GPU.set_pixel(x, y, color[0], color[1], color[2])
+
+
+# funçoes daqui https://www.geeksforgeeks.org/check-whether-a-given-point-lies-inside-a-triangle-or-not/
+def areaTriangle(x1, y1, x2, y2, x3, y3): 
+  
+    return abs((x1 * ((y2) - y3) + x2 * (y3 - y1)  
+                + x3 * (y1 - y2)) / 2.0) 
+
+def isInside(x1, y1, x2, y2, x3, y3, x, y): 
+  
+    A = areaTriangle(x1, y1, x2, y2, x3, y3) 
+
+    A1 = areaTriangle(x, y, x2, y2, x3, y3) 
+      
+    A2 = areaTriangle(x1, y1, x, y, x3, y3) 
+      
+    A3 = areaTriangle(x1, y1, x2, y2, x, y) 
+      
+    if(round(A,4) == round(A1 + A2 + A3, 4)): 
+        return True
+    else:
+        return False
 
 def triangleSet2D(vertices, color):
     """ Função usada para renderizar TriangleSet2D. """
-    gpu.GPU.set_pixel(24, 8, 255, 255, 0) # altera um pixel da imagem
+    color = [color[0]*255, color[1]*255, color[2]*255]
+
+    for x in range(gpu.GPU.width):
+        for y in range(gpu.GPU.height):
+
+            if isInside(vertices[0], vertices[1], vertices[2], vertices[3], vertices[4], vertices[5], x+0.5, y+0.5):
+                gpu.GPU.set_pixel(x, y, color[0], color[1], color[2])
 
 def triangleSet(point, color):
     """ Função usada para renderizar TriangleSet. """
@@ -161,7 +237,15 @@ if __name__ == '__main__':
     # Valores padrão da aplicação
     width = LARGURA
     height = ALTURA
+<<<<<<< HEAD
     x3d_file = "exemplo9.x3d"
+=======
+<<<<<<< HEAD
+    x3d_file = "exemplo4.x3d"
+=======
+    x3d_file = "exemplo3.x3d"
+>>>>>>> p1 done
+>>>>>>> p1 done
     image_file = "tela.png"
 
     # Tratando entrada de parâmetro
